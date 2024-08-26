9b90d24c83787728f74eeb5e63d5e373eab644ed
BryanMtz1217 <bryanmtz1217@gmail.com>
2024-08-26 00:12:28 +0000

    #Commit

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+// This is the HelloWorld class
+public class HelloWorld {
+    // The main() method is the entry point of the program
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+        // Prints my name to the console
+        System.out.println("My Name: Bryan Martinez");
+        // Prints my favorite food to the console
+        System.out.println("Favorite Food: Pizza");
+        // Prints a celebrity or public figure I would like to meet
+        System.out.println("Celebrity to meet: Cristiano Ronaldo");
+        // Prints my least favorite chore to the console
+        System.out.println("Least Favorite Chore: Taking out the trash");
+        // Prints what I would rather be doing right now to the console
+        System.out.println("What I would rather be doing right now: Playing video games");
+    }
+}
+/* Block comments:
+1. The main() method represents the entry point of a Java program.
+It is the first method that is called when it is executed
+2. System.out.Println() is used to print the message to the console
+3. The purpose of comments in the program is to explain the code
+or to help other developers understand what the code does*/
